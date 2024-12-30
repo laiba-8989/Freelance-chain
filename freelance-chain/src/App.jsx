6c222d25fe0e6a3b21741fd5a1f9aafcf37eb947
kashaf-afzalminhas <kashafafzal909@gmail.com>
2024-12-30 15:09:40 +0500

    project created

---
+++ freelance-chain/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+import './App.css'
+import React from 'react'
+function App() {
+  
+  return (
+    <>
+     <h1 className='text-[#1976D2]'>home</h1>
+     
+    </>
+  )
+}
+
+export default App
